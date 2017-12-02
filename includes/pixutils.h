4d4e95a0c1ce8ca45a8546d370ac5cf31bf6971c
Victor "multun" Collod <victor.collod@epita.fr>
2017-12-03 00:47:48 +0100

    pixutils: fix pixel color bitmasks

--- includes/pixutils.h
+++ includes/pixutils.h
@@ -1,8 +1,8 @@
 #include <SDL.h>
 
 #define PIX_R(Pix) ((Pix) & 0xFF)
-#define PIX_G(Pix) ((Pix) & 0xFF << 8)
-#define PIX_B(Pix) ((Pix) & 0xFF << 16)
+#define PIX_G(Pix) (((Pix) >> 8) & 0xFF)
+#define PIX_B(Pix) (((Pix) >> 16) & 0xFF)
 #define PIX_WHITE 0xFFFFFFFF
 
 Uint32 get_pixel(SDL_Surface *surface, int x, int y);
