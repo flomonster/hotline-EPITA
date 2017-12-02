d27de18f317c10f9bb96c24f5bb27c366b1ffc9e
Florian Amsallem <amsall_f@epita.fr>
2017-12-02 16:22:06 +0100

    player: add move

--- src/player.c
+++ src/player.c
@@ -6,6 +6,22 @@
 #include <math.h>
 
 
+static void player_move(s_player *player)
+{
+  float dx = 0;
+  float dy = 0;
+  dy += player->entity.dir & DIR_TOP ? -1 : 0;
+  dy += player->entity.dir & DIR_BOTTOM ? 1 : 0;
+  dx += player->entity.dir & DIR_RIGHT ? 1 : 0;
+  dx += player->entity.dir & DIR_LEFT ? -1 : 0;
+  if (dx || dy)
+  {
+    s_vect dir = vect_mult(vect_normalize(VECT(dx, dy)), player->speed);
+    player->entity.sprite.pos = vect_add(player->entity.sprite.pos, dir);
+  }
+}
+
+
 void player_init(s_player *player, s_renderer *renderer, s_vect pos)
 {
   player->life = 2;
@@ -28,6 +44,23 @@ void player_update(s_player *player, s_game *game)
   double dy = game->input.mouse_pos.y - player->entity.sprite.pos.y;
   double dx = game->input.mouse_pos.x - player->entity.sprite.pos.x;
   player->entity.sprite.angle = atan2(dy, dx) * 180. / M_PI + 90.;
+
+  player->entity.dir = DIR_NONE;
+  if (input_key_pressed(&game->input, SDL_SCANCODE_W))
+    player->entity.dir |= DIR_TOP;
+  if (input_key_pressed(&game->input, SDL_SCANCODE_D))
+    player->entity.dir |= DIR_RIGHT;
+  if (input_key_pressed(&game->input, SDL_SCANCODE_S))
+    player->entity.dir |= DIR_BOTTOM;
+  if (input_key_pressed(&game->input, SDL_SCANCODE_A))
+    player->entity.dir |= DIR_LEFT;
+
+  if ((player->entity.dir & DIR_LEFT) && (player->entity.dir & DIR_RIGHT))
+    player->entity.dir &= ~(DIR_LEFT | DIR_RIGHT);
+  if ((player->entity.dir & DIR_TOP) && (player->entity.dir & DIR_BOTTOM))
+    player->entity.dir &= ~(DIR_BOTTOM | DIR_TOP);
+
+  player_move(player);
 }
 
 
