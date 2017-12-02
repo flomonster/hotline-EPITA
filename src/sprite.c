d9eafa9b3b51f6a56e858f31a0d451378073bc21
Alexandre Kirszenberg <alexandre.kirszenberg@epita.fr>
2017-12-02 14:53:53 +0100

    sprite: correct draw scaling

--- src/sprite.c
+++ src/sprite.c
@@ -38,8 +38,13 @@ void sprite_destroy(s_sprite *s)
 void sprite_draw(s_sprite *s, s_renderer *r)
 {
   s_vect top_left = vect_sub(s->pos, vect_mult(s->size, .5));
-  SDL_Rect src = rect_to_SDL(rect_mult(RECT(VECT(0., 0.), s->size), SCALE_FACTOR), r->sample_factor);
-  SDL_Rect dst = rect_to_SDL(rect_mult(RECT(top_left, s->size), SCALE_FACTOR), r->sample_factor);
+
+  // Scale the destination to the display.
+  s_rect scaled_dst = rect_mult(RECT(top_left, s->size), SCALE_FACTOR);
+
+  // Scale to the sample factor.
+  SDL_Rect src = rect_to_SDL(RECT(VECT(0, 0), s->size), r->sample_factor);
+  SDL_Rect dst = rect_to_SDL(scaled_dst, r->sample_factor);
   SDL_Point ori = vect_to_SDL(s->pos, r->sample_factor);
   SDL_RenderCopyEx(r->renderer, s->texture, &src, &dst,
                    s->angle, &ori, SDL_FLIP_NONE);
