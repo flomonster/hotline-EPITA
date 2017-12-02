77afc482b15ebc1dc597675eaec30ba5fd324c94
Alexandre Kirszenberg <alexandre.kirszenberg@epita.fr>
2017-12-02 11:38:24 +0100

    input: add mouse pos

--- includes/input.h
+++ includes/input.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include "vect.h"
+
 #include <SDL.h>
 #include <stdbool.h>
 
@@ -8,9 +10,11 @@
 typedef struct input
 {
   bool keys[1024];
+  s_vect mouse_pos;
 } s_input;
 
 
 void input_init(s_input *input);
 bool input_pressed(s_input *input, SDL_Keycode code);
 void input_handle_event(s_input *input, SDL_Event *event);
+s_vect input_mouse_pos(s_input *input);
--- src/input.c
+++ src/input.c
@@ -7,6 +7,8 @@
 void input_init(s_input *input)
 {
   memset(input->keys, 0, sizeof (input->keys));
+  input->mouse_pos.x = 0;
+  input->mouse_pos.y = 0;
 }
 
 
@@ -25,7 +27,17 @@ void input_handle_event(s_input *input, SDL_Event *event)
   case SDL_KEYUP:
     input->keys[event->key.keysym.scancode] = false;
     break;
+  case SDL_MOUSEMOTION:
+    input->mouse_pos.x = event->motion.x;
+    input->mouse_pos.y = event->motion.y;
+    break;
   default:
     break;
   }
+}
+
+
+s_vect input_mouse_pos(s_input *input)
+{
+  return input->mouse_pos;
 }
